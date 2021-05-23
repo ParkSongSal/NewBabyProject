9c6a395020de9de40a1b1f2b02e23d22ad65b727
ParkSongSal <tkdanr2427@naver.com>
2021-05-23 23:34:31 +0900

    login.xml 화면 작성

---
+++ app/src/main/java/com/example/newbabyproject/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.example.newbabyproject
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun onClick(view: View) {
+
+        /*when (view.id) {
+            R.id.loginBtn -> Login()
+            R.id.registerTxt -> Register()
+            R.id.inquireTxt -> Inquire()
+        }*/
+    }
+}
