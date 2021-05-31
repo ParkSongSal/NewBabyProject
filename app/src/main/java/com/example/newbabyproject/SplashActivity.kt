84c625ce4437752920f386f9f1e672e1b78391b6
ParkSongSal <tkdanr2427@naver.com>
2021-05-31 20:52:58 +0900

    # 로딩화면 추가

---
+++ app/src/main/java/com/example/newbabyproject/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.newbabyproject
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.example.newbabyproject.utils.Common
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        startLoading()
+    }
+
+    private fun startLoading() {
+        val handler = Handler()
+        handler.postDelayed({
+            Common.intentCommon(this@SplashActivity, LoginActivity::class.java)
+            finish()
+        }, 2000) // 2.5초후에 실행
+    }
+}
