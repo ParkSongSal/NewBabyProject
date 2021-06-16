3b05e739ef32da28dd2bb0434702eead6ee28839
ParkSongSal <tkdanr2427@naver.com>
2021-06-16 22:19:28 +0900

    # 입원 안내문 Toolbar 적용 # 퇴원 안내문 Toolbar 적용

--- app/src/main/java/com/example/newbabyproject/OutIntroduceActivity.kt
+++ app/src/main/java/com/example/newbabyproject/OutIntroduceActivity.kt
@@ -1,18 +1,27 @@
 package com.example.newbabyproject
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_app_introduce.*
+import kotlinx.android.synthetic.main.item_toolbar.*
 
 class OutIntroduceActivity : BaseActivity() {
 
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_out_introduce)
+
+
+        toolbar.setTitleTextColor(getColor(R.color.whiteColor))
+        toolbar.title = "퇴원 안내문"
+        setSupportActionBar(toolbar)
 
         init(applicationContext)
         introduceValidate("2")  // 퇴원 안내문
